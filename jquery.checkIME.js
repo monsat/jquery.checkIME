a3a0e9518160ce93207e5e1154cbe18e768dcb13
mon_sat <tanaka.kohji+github@gmail.com>
2012-05-13 18:08:32 +0900

    fixed status

--- jquery.checkIME.js
+++ jquery.checkIME.js
@@ -4,30 +4,40 @@
 		this.options = $.extend({}, $.fn.checkIME.defaults, options);
 		// check
 		var keypressCount = 0;
+		var confirm = false;
 		var checkImeModeOn = function(e) {
 			var $this = $(this);
-			var status = 0;
+			var data = { status: 0, keyCode: e.keyCode};
+			var triggers = [];
 			if (e.type == 'keypress' && (e.keyCode != 241) && (e.keyCode != 242)) {
 				keypressCount++;
 			} else if (e.type == 'keyup') {
 				keypressCount--;
+				triggers.push('checkIME.keyup');
 				if ( keypressCount < 0 ) {
 					if (e.keyCode == 13) {
-						status = 9; // confirmed conversion
-						$this.triggerHandler('checkIME.confirmed', {status: status});
+						data.status = -1; // confirmed conversion
+						confirm = true;
+						triggers.push('checkIME.confirmed');
+					} else if (!confirm) {
+						data.status = 1; // in progress
+						triggers.push('checkIME.progress');
 					} else {
-						status = 1; // in progress
-						$this.triggerHandler('checkIME.progress', {status: status});
+						confirm = false;
 					}
+					triggers.push('checkIME.imeon');
 					keypressCount = 0;
-					$this.triggerHandler('checkIME.imeon', {status: status});
 				} else {
-					status = 0; // ime-mode off
-					$this.triggerHandler('checkIME.imeoff', {status: status});
+					data.status = 0; // ime-mode off
+					triggers.push('checkIME.imeoff');
 				}
 			}
-			$this.attr('data-checkime', status);
-			return status;
+			$this.attr('data-checkime', data.status);
+			// triger
+			$.each(triggers, function(i, t){
+				$this.triggerHandler(t, data);
+			});
+			return data.status;
 		}
 		// bind
 		$(element).keyup(checkImeModeOn)
